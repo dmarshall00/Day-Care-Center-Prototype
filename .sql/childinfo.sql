5f749e555c6a716a8d75870a5bd2b1d118f9f71a
dmarshall00 <mdawn652@gmail.com>
2024-04-09 14:18:59 -0400

    Make table

---
+++ .sql/childinfo.sql
@@ -0,0 +1,10 @@
+CREATE TABLE ChildInfo (
+    ParentID varchar(9),
+    ChildID varchar(9),
+    FacultyID varchar(9),
+    LastName varchar(50),
+    FirstName varchar(50),
+    Age Int,
+    Medication varchar(255),
+    Address varchar(255)
+);
