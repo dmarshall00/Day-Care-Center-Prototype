335cacab0428a02de53dd1314bcc9f914cfd4151
dmarshall00 <mdawn652@gmail.com>
2024-04-09 14:24:32 -0400

    Make table

---
+++ .sql/facultyinfo.sql
@@ -0,0 +1,10 @@
+CREATE TABLE FacultyInfo (
+    FacultyID varchar(9),
+    ChildID varchar(9),
+    LocationID varchar(4),
+    DepartmentID varchar(4),
+    LastName varchar(50),
+    FirstName varchar(50),
+    PhoneNumber varchar(12),
+    Email varchar(60)
+);
