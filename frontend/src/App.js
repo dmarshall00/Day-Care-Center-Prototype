1fd2a02c9109c21b4f7a2852484ee67898cd519c
Uzo45 <uanaele34@gmail.com>
2024-04-12 15:00:30 -0400

    Parent Log in and Sign in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import Login from './ParentLogin'
+import ParentLogin from './ParentLogin'
+import ParentSignup from './ParentSignup'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ParentLogin />}></Route>
+        <Route path='/signup' element={<ParentSignup />}></Route>
+      </Routes>
+      <ParentLogin />
+    </BrowserRouter>
   );
 }
 
--- frontend/src/ParentSignup.js
+++ frontend/src/ParentSignup.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-function ParentLogin() {
+function ParentSignup() {
     return(
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <h2>Parent Log in</h2>
+                <h2>Parent Sign In</h2>
                 <form action=''>
+                    <div className='mb-3'>
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder="Email" className='form-control rounded-0'/>
+                    </div>
+
                     <div className='mb-3'>
                         <label htmlFor="username"><strong>Username</strong></label>
                         <input type="username" placeholder="Username" className='form-control rounded-0'/>
@@ -17,13 +22,13 @@ function ParentLogin() {
                         <input type="password" placeholder="Password" className='form-control rounded-0'/>
                     </div>
 
-                    <button className='btn btn-success w-100 rounded-0'><strong>Log in</strong></button>
-                    <p>Words that means something</p>
-                    <Link to="/signup" className='btn btn-default border w-100 bg-light'><strong>Create Account</strong></Link>
+                    <button className='btn btn-success w-100 rounded-0'><strong>Sign up</strong></button>
+                    <p>Words that means something else</p>
+                    <Link to="/" className='btn btn-default border w-100 bg-light'><strong>Log in</strong></Link>
                 </form>
             </div>
         </div>
     );
 }
 
-export default ParentLogin;
+export default ParentSignup;
